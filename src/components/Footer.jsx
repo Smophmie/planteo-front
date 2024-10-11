2653c928e5d7ab0f1e34112476886ea042d1d0f3
Sophie Théréau <sophiethereau@hotmail.fr>
2024-10-11 13:44:46 +0200

    Ajout de mon contact dans le footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,9 +15,9 @@ const navLinksConnected = [
 
 function Footer ({ isAuthenticated, isAdmin }) {
 
-    return <footer className="m-4">
+    return <footer className="mx-3 my-2">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-            <div className="sm:flex sm:items-center sm:justify-between">
+            <div className="sm:flex sm:items-start sm:justify-between">
                 <Link to="/" className="">
                     <img
                     src="/img/logo-removebg-preview.png"
@@ -25,18 +25,28 @@ function Footer ({ isAuthenticated, isAdmin }) {
                     className="logo"
                     />
                 </Link>
-                <ul className="menu menu-horizontal mb-3 sm:mb-0">
-                    {!isAuthenticated && navLinks.map( (navLink) => <li key={navLink.name}><a href={navLink.href} >{navLink.name}</a></li>)}
-                    {isAuthenticated && navLinksConnected.map( (navLink) => <li key={navLink.name}><a href={navLink.href} >{navLink.name}</a></li>)}   
-                    {isAdmin && <li><a href='/dashboard'>Tableau de bord</a></li>}                 
-                </ul>
-                <ul className="menu menu-horizontal mb-3 sm:mb-0">
-                    <li><a href="/legals" className="">Mentions légales</a></li>
-                    <li><a href="/privacy-policy" className="">Politique de confidentialité</a></li>
-                    <li><a href="/" className="">Contact</a></li>
-                </ul>
+                <div className='menu mb-3 sm:mb-0 items-center justify-center'>
+                    <ul className="md:menu-horizontal md:flex-row sm:flex-col justify-center">
+                        {!isAuthenticated && navLinks.map( (navLink) => <li key={navLink.name} className='items-center'><a href={navLink.href}>{navLink.name}</a></li>)}
+                        {isAuthenticated && navLinksConnected.map( (navLink) => <li key={navLink.name} className='items-center'><a href={navLink.href}>{navLink.name}</a></li>)}   
+                        {isAdmin && <li className='items-center'><a href='/dashboard'>Tableau de bord</a></li>}                 
+                    </ul>
+                    <ul className="menu-horizontal items-end mt-4">
+                        <li><a href="mailto:sophiethereau@hotmail.fr">Me joindre par e-mail</a></li>
+                        <li><a href="tel:+33648437627">Me joindre par téléphone</a></li>
+                    </ul>
+                </div>
+                <div className='menu mb-3 sm:mb-0 space-y-4'>
+                    <ul className="menu-horizontal">
+                        <li><a href="/legals" className="">Mentions légales</a></li>
+                        <li><a href="/privacy-policy" className="">Politique de confidentialité</a></li>
+                    </ul>
+                    
+                </div>
+                
             </div>
         </div>
+        
     </footer>
     
     
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import HeroSection from "../components/HeroSection";
+
+function Contact () {
+    return <>
+        <HeroSection title="Contactez moi"/>
+           
+    </>
+}
+
+export default Contact;
