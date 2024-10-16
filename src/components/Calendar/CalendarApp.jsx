6dfac5276e9eda52b7527d745474728f42b3fedc
Sophie Théréau <sophiethereau@hotmail.fr>
2024-10-16 10:37:42 +0200

    Ajout du calendrier d'évènements

--- src/components/Calendar/CalendarApp.jsx
+++ src/components/Calendar/CalendarApp.jsx
@@ -4,37 +4,53 @@ import {
   createViewMonthGrid,
 } from '@schedule-x/calendar'
 import { createEventsServicePlugin } from '@schedule-x/events-service'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
  
 import '@schedule-x/theme-default/dist/index.css'
 import { createEventModalPlugin } from '@schedule-x/event-modal'
 import { createDragAndDropPlugin } from '@schedule-x/drag-and-drop'
-import { fr } from 'date-fns/locale';
-
+import axios from 'axios'
  
 function CalendarApp() {
-    const plugins = [
-        createEventsServicePlugin(),
-        createEventModalPlugin(),
-        createDragAndDropPlugin(),
-      ];
+  const plugins = [
+      createEventsServicePlugin(),
+      createEventModalPlugin(),
+      createDragAndDropPlugin(),
+    ];
+  
+  const [events, setEvents] = useState([]);
  
+  const fetchEvents = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await axios.get(`${import.meta.env.VITE_BACK_URL_LARAVEL}events`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(response.data);
+      setEvents(response.data);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des événements:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEvents();
+    console.log(events);
+  }, []);
+
   const calendar = useCalendarApp({
     views: [createViewMonthGrid(), createViewMonthAgenda()],
-    events: [
-      {
-        id: '1',
-        title: 'Evènement 1',
-        start: '2024-10-15',
-        end: '2024-10-15',
-      },
-    ],
+    events: events,
     locale: 'fr-FR',
   }, plugins)
  
   useEffect(() => {
-    calendar.eventsService.getAll()
-  }, [])
+    if (events.length > 0) {
+      calendar.eventsService.set(events);
+    }
+  }, [events, calendar.eventsService]);
  
   return (
     <div className='w-3/4 my-10 mx-auto'>
